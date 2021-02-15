c8f289d5d4481731ea20d0122da721f081872441
xiaoqiang-zhao <longze_xq@163.com>
2021-02-15 10:49:14 +0800

    添加 yi

--- src/pages/uploadInfo/index.vue
+++ src/pages/uploadInfo/index.vue
@@ -1,7 +1,16 @@
 <template>
     <section class="upload-info">
         <process-statistics/>
-        信息上传开发中，敬请期待...
+        <section class="common-block-section">
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="一般纳税人" name="first">
+                    用户管理
+                </el-tab-pane>
+                <el-tab-pane label="小规模纳税人" name="second">
+                    配置管理
+                </el-tab-pane>
+            </el-tabs>
+        </section>
     </section>
 </template>
 
@@ -14,8 +23,17 @@ import processStatistics from '@/components/process-statistics';
 export default {
     components: {
         processStatistics
+    },
+    data() {
+        return {
+            activeName: 'second'
+        };
+    },
+    methods: {
+        handleClick(tab, event) {
+            console.log(tab, event);
+        }
     }
-    
 }
 </script>
 
