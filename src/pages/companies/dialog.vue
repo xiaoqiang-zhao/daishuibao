8ae9945acbe186e85d9bb4ade8fcf791dde2492f
xiaoqiang-zhao <longze_xq@163.com>
2021-02-21 14:50:05 +0800

    初始化公司编辑界面

--- src/pages/companies/dialog.vue
+++ src/pages/companies/dialog.vue
@@ -1,11 +1,11 @@
 <template>
     <el-dialog
-        v-if="companyData"
         :title="title"
         :visible.sync="dialogVisible"
         width="800px">
         <section>
             <el-form
+                v-if="companyData"
                 ref="form"
                 :model="form"
                 label-width="120px"
@@ -25,19 +25,19 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item label="会计负责人">
-                    <el-input v-model="form.companyName" placeholder="请输入"></el-input>
+                    <el-input v-model="form.accountingManager" placeholder="请输入"></el-input>
                 </el-form-item>
                 <el-form-item label="手机号">
-                    <el-input v-model="form.companyName" placeholder="请输入"></el-input>
+                    <el-input v-model="form.mobile" placeholder="请输入"></el-input>
                 </el-form-item>
                 <el-form-item label="微信号">
-                    <el-input v-model="form.companyName" placeholder="请输入"></el-input>
+                    <el-input v-model="form.weChartAccount" placeholder="请输入"></el-input>
                 </el-form-item>
                 <el-form-item label="营业执照号">
-                    <el-input v-model="form.companyName" placeholder="请输入"></el-input>
+                    <el-input v-model="form.businessLicenseNumber" placeholder="请输入"></el-input>
                 </el-form-item>
                 <el-form-item label="纳税类型">
-                    <el-select v-model="form.type" placeholder="请选择">
+                    <el-select v-model="form.payTaxesType" placeholder="请选择">
                         <el-option
                             v-for="item in payTaxesTypeList"
                             :key="item"
@@ -47,58 +47,60 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item label="电子税务局密码">
-                    <el-input v-model="form.companyName" placeholder="请输入"></el-input>
+                    <el-input v-model="form.password" placeholder="请输入"></el-input>
                 </el-form-item>
-                <el-form-item label="初期数">
-                    <div>
+                <template v-if="type === 'add'">
+                    <el-form-item label="资产负债表">
                         <el-upload
                             action="https://jsonplaceholder.typicode.com/posts/"
                             class="upload-item">
                             <el-button size="small" type="primary">点击上传【资产负债表】excel</el-button>
                         </el-upload>
+                    </el-form-item>
+                    <el-form-item label="利润表">
                         <el-upload
                             action="https://jsonplaceholder.typicode.com/posts/"
                             class="upload-item">
                             <el-button size="small" type="primary">点击上传【利润表】excel</el-button>
                         </el-upload>
+                    </el-form-item>
+                    <el-form-item label="银行流水表">
                         <el-upload
                             action="https://jsonplaceholder.typicode.com/posts/"
                             class="upload-item">
                             <el-button size="small" type="primary">点击上传【银行流水表】excel</el-button>
                         </el-upload>
-                        <div class="explain">
-                            若客户为老客户，则请将上一财务周期的报表上传至此；若客户为新客户，则忽略。
-                        </div>
-                    </div>
-                </el-form-item>
+                    </el-form-item>
+                </template>
             </el-form>
         </section>
-        <!-- <section class="top-section">
-            <div>{{ companyData.companyName }}</div>
-            <div class="explain">
-                <div>
-                    1. 请务必保证表格内公司名称与发票上名称相同，否则将影响会计凭证生成
+        <section class="suppliers-and-customers">
+            <section class="top-section">
+                <div class="explain">
+                    <div>
+                        1. 请务必保证表格内公司名称与发票上名称相同，否则将影响会计凭证生成
+                    </div>
+                    <div>
+                        2. 可从Excel复制内容粘贴至表格
+                    </div>
+                    <div>
+                        3. 该名单将根据每月发票信息自动更新
+                    </div>
                 </div>
-                <div>
-                    2. 可从Excel复制内容粘贴至表格
-                </div>
-                <div>
-                    3. 该名单将根据每月发票信息自动更新
-                </div>
-            </div>
+            </section>
+            <section class="tabs-container">
+                <el-tabs value="suppliers" class="tab suppliers">
+                    <el-tab-pane label="供应商库" name="suppliers">
+                        供应商库
+                    </el-tab-pane>
+                </el-tabs>
+                <el-tabs value="customers" class="tab customers">
+                    <el-tab-pane label="客户库" name="customers">
+                        供应商库
+                    </el-tab-pane>
+                </el-tabs>
+            </section>
         </section>
-        <section class="tabs-container">
-            <el-tabs value="suppliers" class="tab suppliers">
-                <el-tab-pane label="供应商库" name="suppliers">
-                    供应商库
-                </el-tab-pane>
-            </el-tabs>
-            <el-tabs value="customers" class="tab customers">
-                <el-tab-pane label="客户库" name="customers">
-                    供应商库
-                </el-tab-pane>
-            </el-tabs>
-        </section> -->
         <div slot="footer" class="dialog-footer">
             <el-button @click="dialogVisible = false">取 消</el-button>
             <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
@@ -108,6 +110,8 @@
 
 <script>
 import map from '@/components/map.js';
+import 'element-ui/lib/theme-chalk/upload.css';
+
 export default {
     props: {
     },
@@ -115,19 +119,23 @@ export default {
         return {
             dialogVisible: false,
             title: '',
+            type: '',
             companyData: null,
             industryList: map.industryList,
             payTaxesTypeList: map.payTaxesTypeList,
             form: {
                 companyName: '',
-                type: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
-                desc: ''
-            }
+                industry: '',
+                accountingManager: '',
+                mobile: '',
+                weChartAccount: '',
+                businessLicenseNumber: '',
+                payTaxesType: '',
+                password: '',
+                suppliers: [],
+                customers: []
+            },
+            rules: {}
         };
     },
     methods: {
@@ -136,20 +144,26 @@ export default {
          * 打开弹框
          */
         open(companyData) {
-            // debugger
             this.dialogVisible = true;
-            // this.companyData = companyData;
-            // // 编辑
-            // if (companyData) {
-            //     this.title = companyData.companyName;
-            // }
-            // // 新建
-            // else {
-            //     this.title = '新建客户';
-            // }
+            this.companyData = companyData || {};
+            // 编辑
+            if (companyData) {
+                this.type = 'edit';
+                this.title = companyData.companyName;
+                // 回填数据
+                // this.form
+            }
+            // 新建
+            else {
+                this.type = 'add';
+                this.title = '新建客户';
+            }
         },
 
-        loadData() {
+        /**
+         * 提交数据
+         */
+        postData() {
             // this.$http.get(`/companies/${this.companyData.id}/suppliers-and-customers`).then(res => {
             //     console.log(res.data);
             // });
@@ -168,32 +182,39 @@ export default {
         flex: 1;
         min-width: 47%;
         max-width: 47%;
+        .el-select,
+        .el-upload {
+            display: block;
+        }
     }
 }
-.top-section {
-    margin-top: -20px;
+.suppliers-and-customers {
+    padding: 20px 46px 0 39px;
+    .top-section {
+        margin-top: -20px;
 
-    .explain {
-        padding-top: 5px;
-        color: @color-grey-low;
-        font-size: 12px;
-        line-height: 1.2em;
-    }
-}
-
-.tabs-container {
-    display: flex;
-
-    .tab {
-        flex: 1;
+        .explain {
+            padding-top: 5px;
+            color: @color-grey-low;
+            font-size: 12px;
+            line-height: 1.2em;
+        }
     }
 
-    .suppliers {
-        margin-right: 10px;
-    }
+    .tabs-container {
+        display: flex;
 
-    .customers {
-        margin-left: 10px;
+        .tab {
+            flex: 1;
+        }
+
+        .suppliers {
+            margin-right: 10px;
+        }
+
+        .customers {
+            margin-left: 10px;
+        }
     }
 }
 </style>
