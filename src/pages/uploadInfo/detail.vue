e2c0cfa700933cb5d2490ecf8d02551e85347fe3
xiaoqiang-zhao <longze_xq@163.com>
2021-02-24 21:53:33 +0800

    信息上传 / 发票与银行回单上传

--- src/pages/uploadInfo/detail.vue
+++ src/pages/uploadInfo/detail.vue
@@ -79,7 +79,8 @@
                         <el-upload
                             class="upload-demo"
                             drag
-                            action="https://jsonplaceholder.typicode.com/posts/"
+                            accept=".xlsx,.xls"
+                            action="/api/upload/invoice"
                             multiple>
                             <i class="el-icon-upload"></i>
                             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
@@ -97,7 +98,8 @@
                         <el-upload
                             class="upload-demo"
                             drag
-                            action="https://jsonplaceholder.typicode.com/posts/"
+                            accept=".xlsx,.xls"
+                            action="/api/upload/bankSlip"
                             multiple>
                             <i class="el-icon-upload"></i>
                             <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
